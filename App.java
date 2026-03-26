33e7414b81f83fb5b2eb435df1a4bd6d7a4c87b8
Hamza <hamzaelfaour9@gmail.com>
2026-03-26 09:34:32 +0100

    Update App.java
    Menu: add 'Add' + choice prompt

--- App.java
+++ App.java
@@ -1,5 +1,7 @@
 public class App{
     static void run(){
-        System.out.println("Calculator v0.1");
+        System.out.println("1) Add/n0) Exist;
+        System.out.println("Choice");
+        Strig choice = new java.until.Scanner.(System.in).nextLine
     }
 }
