c139ce9d536955109e47863b30a42750b08f8df7
Hamza <hamzaelfaour9@gmail.com>
2026-03-26 10:14:45 +0100

    Update App.java
    Handle choice 1 stub

--- App.java
+++ App.java
@@ -1,10 +1,12 @@
 public class App {
     static void run(){
-        System.out.println("1) Subtract/n0) Exit;
+        System.out.println("1) Subtract\n0) Exit;
         System.out.println("Your choice");
         String choice = new java.util.Scanner.(System.in).nextLine();
         if ("1".equals(choice))
             System.out.println("1) Add \n2) Subtract \n3) Multiply \n4) Divide \n0) Exit");
+        if ("1".equals(choice))
+            System.out.println("TODO Add");
         
     }
 }
