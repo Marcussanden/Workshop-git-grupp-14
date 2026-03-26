4c563af32530e051928d2ae1b80c1468de377064
Marcussanden <mackansan@gmail.com>
2026-03-26 09:43:46 +0100

    Add calculator.add(a,b)

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ public class App{
     static void run(){
         System.out.println("1) Subtract/n0) Exit;
         System.out.println("Your choice");
-        Strig choice = new java.until.Scanner.(System.in).nextLine();
+        String choice = new java.until.Scanner.(System.in).nextLine();
     }
 }
