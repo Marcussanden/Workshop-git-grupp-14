4f1e2dbb1c5c2b6eeb73c94a79f5a6504df3d50b
Hamza <hamzaelfaour9@gmail.com>
2026-03-26 10:36:37 +0100

    Update App.java

--- App.java
+++ App.java
@@ -8,9 +8,6 @@ public class App {
             System.out.println("1) Add \n2) Subtract \n3) Multiply \n4) Divide \n0) Exit");
         if ("1".equals(choice))
 
-        if("3".equals(choice))
-            System.out.println(Calculator.mul(a,b));
-
         double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         double b = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
 
@@ -18,6 +15,9 @@ public class App {
 
         if("2".equals(choice))
         System.out.println(Calculator.sub(a, b)));
+
+        if("3".equals(choice))
+            System.out.println(Calculator.mul(a,b));
 
         if("4".equals(choice)) System.out.println(Calculator.div(a, b))
 
